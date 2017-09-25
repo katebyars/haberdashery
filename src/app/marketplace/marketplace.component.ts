3d5fb97fd1fb076a560eaca643266bd539a43add
Esti Shay <esti.r.shay@gmail.com>
2017-09-25 16:09:25 -0700

    implement services lesson

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,27 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [ProductService]
 })
-export class MarketplaceComponent {
-  constructor(private router: Router){}
+export class MarketplaceComponent implements OnInit {
+  products: Product[];
 
-  products: Product[] = [
-    new Product("Hat", "Sugar Blues", "Goorin Bros", 140, 1),
-    new Product("Hat", "White Rabbit", "Goorin Bros", 130, 2),
-    new Product("Hat", "JB Harlow", "Goorin Bros", 60, 3),
-    new Product("Hat", "Papou", "Goorin Bros", 70, 4),
-    new Product("Hat", "Mrs. Blanc", "Goorin Bros", 60, 5),
-    new Product("Hat", "Becky Jones Cloche", "Goorin Bros", 55, 6),
-    new Product("Accessory", "Goorin Hat Box Medium", "Goorin Bros", 14, 7),
-    new Product("Bulk Coffee", "Tanganyika Blend", "Missionary's Choice", 12, 8),
-    new Product("Bulk Tea", "Frishtik Black", "Rusofsky Brewers", 9, 9),
-    new Product("Tea Cookies", "Madelines", "Allons-y", 70, 10),
-  ];
+  constructor(private router: Router, private productService: ProductService){}
+
+  ngOnInit(){
+    this.products = this.productService.getProducts();
+  }
 
   gotToDetailPage(clickedProduct: Product){
     this.router.navigate(['products', clickedProduct.id]);
---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { PRODUCTS } from './mock-products';
+
+@Injectable()
+export class ProductService {
+
+  constructor() { }
+
+  getProducts(){
+    return PRODUCTS;
+  }
+
+  getProductById(productId: number){
+    for (var i=0; i <= PRODUCTS.length -1; i++){
+      if (PRODUCTS[i].id === productId){
+        return PRODUCTS[i];
+      }
+    }
+  }
+
+}
